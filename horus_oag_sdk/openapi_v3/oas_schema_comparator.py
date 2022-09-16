5061e3c729e167ad82956a3d0c7c9e20a2e47156
jc42c <juancarlos.dealfonso@42crunch.com>
2022-09-16 17:14:14 +0200

    working default responses plugin part

--- horus_oag_sdk/openapi_v3/oas_schema_comparator.py
+++ horus_oag_sdk/openapi_v3/oas_schema_comparator.py
@@ -1,5 +1,7 @@
+from .oas_schema_searcher import search_reference_content
 
-def same_content(content_1: dict, content_2: dict) -> bool:
+
+def same_content(oas: dict, content_1: dict, content_2: dict) -> bool:
     """Compare two contents and return True if they are EQUIVALENT"""
 
     if len(content_1) != len(content_2):
@@ -9,14 +11,26 @@ def same_content(content_1: dict, content_2: dict) -> bool:
         if not content_type in content_2:
             return False
 
-        if not same_schema(content_1[content_type], content_2[content_type]):
+        if not same_schema(oas, content_1[content_type], content_2[content_type]):
             return False
 
     return True
 
 
-def same_schema(sch1: dict, sch2: dict) -> bool:
+def same_schema(oas: dict, sch1: dict, sch2: dict) -> bool:
     """Compare two schemas and return True if they are EQUIVALENT"""
+
+    if 'schema' in sch1:
+        sch1 = sch1['schema']
+
+    if 'schema' in sch2:
+        sch2 = sch2['schema']
+
+    if "$ref" in sch1:
+        sch1 = search_reference_content(oas, sch1["$ref"])
+
+    if "$ref" in sch2:
+        sch2 = search_reference_content(oas, sch2["$ref"])
 
     if not 'type' in sch1 or not 'type' in sch2:
         return False
@@ -35,14 +49,14 @@ def same_schema(sch1: dict, sch2: dict) -> bool:
             if key not in properties_2:
                 return False
 
-            if not same_schema(value, properties_2[key]):
+            if not same_schema(oas, value, properties_2[key]):
                 return False
 
     elif sch1['type'] == 'array':
         items_1 = sch1.get('items', {})
         items_2 = sch2.get('items', {})
 
-        if not same_schema(items_1, items_2):
+        if not same_schema(oas, items_1, items_2):
             return False
 
     elif sch1['type'] == 'string':
