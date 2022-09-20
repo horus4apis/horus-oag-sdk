fdcf8cd4034b1a63511030402b330780bcd89287
jc42c <juancarlos.dealfonso@42crunch.com>
2022-09-20 10:19:50 +0200

    working security discovery

--- horus_oag_sdk/openapi_v3/oas_common.py
+++ horus_oag_sdk/openapi_v3/oas_common.py
@@ -1,5 +1,3 @@
-
-
 def get_path(oas: dict, path: str) -> dict:
     return oas.get('paths', {}).get(path, {})
 
@@ -17,9 +15,26 @@ def get_oas_parameters(oas: dict, path: str, method: str) -> list:
 
 
 def get_request_schema(oas: dict, path: str, method: str, request_type: str) -> dict:
-    return oas.get('paths', {}).get(path, {}).get(method, {}).get('requestBody', {}).get('content', {}).get(request_type, {}).get('schema', {})
+    return oas.get('paths', {}).get(path, {}).get(method, {}).get('requestBody', {}).get('content', {}).get \
+        (request_type, {}).get('schema', {})
 
 
 def get_response_schema(oas: dict, path: str, method: str, rc: str, response_type: str) -> dict:
-    return oas.get('paths', {}).get(path, {}).get(method, {}).get('responses', {}).get(rc, {}).get('content', {}).get(response_type, {}).get('schema', {})
+    return oas.get('paths', {}).get(path, {}).get(method, {}).get('responses', {}).get(rc, {}).get('content', {}).get \
+        (response_type, {}).get('schema', {})
 
+
+def get_components(oas: dict) -> dict:
+    return oas.get('components', {})
+
+
+def get_schemas(oas: dict) -> dict:
+    return oas.get('components', {}).get('schemas', {})
+
+
+def set_empty_security_schemes(oas: dict):
+    oas['components']['securitySchemes'] = {}
+
+
+def get_security_schemes(oas: dict) -> dict:
+    return oas.get('components', {}).get('securitySchemes', {})
